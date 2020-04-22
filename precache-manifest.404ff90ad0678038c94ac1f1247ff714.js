a05ba906a113d41e940e5f88f3decc5809ac57a2
Ihor Sudenko <sudenkoweb@gmail.com>
2020-04-22 04:44:34 +0300

    Updates

---
+++ precache-manifest.404ff90ad0678038c94ac1f1247ff714.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1e4816e64707ea38e7a7499ad48ec8",
+    "url": "/top-rec/index.html"
+  },
+  {
+    "revision": "a2320df5b1a382c79975",
+    "url": "/top-rec/static/css/main.844caf71.chunk.css"
+  },
+  {
+    "revision": "f4f3a9bc8692467232eb",
+    "url": "/top-rec/static/js/2.0588a777.chunk.js"
+  },
+  {
+    "revision": "8337da4407636c7c138d79f12e2ad8f1",
+    "url": "/top-rec/static/js/2.0588a777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2320df5b1a382c79975",
+    "url": "/top-rec/static/js/main.81264f5b.chunk.js"
+  },
+  {
+    "revision": "4556b4d0f2a0aed29173",
+    "url": "/top-rec/static/js/runtime-main.0ed5363b.js"
+  },
+  {
+    "revision": "ae64cc9c31d24bf63d2c3ed239e43e46",
+    "url": "/top-rec/static/media/arrow-right.ae64cc9c.svg"
+  },
+  {
+    "revision": "68a979945d9cf4bdd59da413eb694e2a",
+    "url": "/top-rec/static/media/icon-like-checked.68a97994.svg"
+  },
+  {
+    "revision": "a94f9a3bb83ac016bf458a8cb6e7492e",
+    "url": "/top-rec/static/media/icon-like.a94f9a3b.svg"
+  },
+  {
+    "revision": "3aa9827b01c85834a233b26c32715a8a",
+    "url": "/top-rec/static/media/icon-search.3aa9827b.svg"
+  },
+  {
+    "revision": "a996fd351c1d4b19e9aa9c39b7d575dc",
+    "url": "/top-rec/static/media/logo.a996fd35.svg"
+  },
+  {
+    "revision": "71c1cc0a4b8d0c725329516be454cb72",
+    "url": "/top-rec/static/media/top100.71c1cc0a.jpg"
+  }
+]);
